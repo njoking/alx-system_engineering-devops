a0a37a2ef510a3b525614c7e4e7ad3c2b25e2738
njoking <njokingugi9462@gmail.com>
2024-04-11 21:19:35 +0300

    webstack monitoring

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,21 @@
-# Fixes a faulty wordpress site
-exec { 'fix-wordpress':
-  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
-/var/www/html/wp-settings.php; service apache2 restart"',
-  path    => '/usr/bin:/usr/sbin:/bin'
+# Define a file resource to replace the faulty line in wp-settings.php
+file { '/var/www/html/wp-settings.php':
+  ensure  => file,
+  content => file('/var/www/html/wp-settings.php').content.gsub('class-wp-locale.phpp', 'class-wp-locale.php'),
+  require => Exec['strace_wp_fix'],
+}
+
+# Define an exec resource to restart Apache after fixing the file
+exec { 'restart_apache':
+  command     => '/bin/systemctl restart apache2',
+  path        => '/usr/bin:/usr/sbin:/bin',
+  refreshonly => true,
+  subscribe   => File['/var/www/html/wp-settings.php'],
+}
+
+# Define an exec resource to attach strace to Apache process and look for 500 error
+exec { 'strace_wp_fix':
+  command     => 'strace -p $(pgrep apache2) -f -e trace=open,write,connect,accept',
+  path        => '/usr/bin:/usr/sbin:/bin',
+  refreshonly => true,
 }
