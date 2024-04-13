0cdbbcc906cf84e9a09b3934e2c12caa20e0b670
njoking <njokingugi9462@gmail.com>
2024-04-13 08:14:45 -0700

    alright

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,21 +1,6 @@
-# Define a file resource to replace the faulty line in wp-settings.php
-file { '/var/www/html/wp-settings.php':
-  ensure  => file,
-  content => file('/var/www/html/wp-settings.php').content.gsub('class-wp-locale.phpp', 'class-wp-locale.php'),
-  require => Exec['strace_wp_fix'],
-}
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-# Define an exec resource to restart Apache after fixing the file
-exec { 'restart_apache':
-  command     => '/bin/systemctl restart apache2',
-  path        => '/usr/bin:/usr/sbin:/bin',
-  refreshonly => true,
-  subscribe   => File['/var/www/html/wp-settings.php'],
-}
-
-# Define an exec resource to attach strace to Apache process and look for 500 error
-exec { 'strace_wp_fix':
-  command     => 'strace -p $(pgrep apache2) -f -e trace=open,write,connect,accept',
-  path        => '/usr/bin:/usr/sbin:/bin',
-  refreshonly => true,
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
