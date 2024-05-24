b3f42de9e0253af3e56d45852471c3b6d204bcea
njoking <njokingugi9462@gmail.com>
2024-05-24 03:41:03 -0700

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,24 @@
-#automate nginx using puppet
+# Setup New Ubuntu server with nginx
 
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
 
 package { 'nginx':
-	ensure => installed,
-}
-file_line { 'install':
-	ensure => 'present',
-	path => '/etc/nginx/sites-enabled/default',
-	after => 'listen 80 default_server;',
-	line => 'rewrite ^/redirect_me https://www.github.com/njoking permnent;',
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-file { '/var/www/html/index.html':
+file {'/var/www/html/index.html':
 	content => 'Hello World!'
-	}
-service { 'nginx' :
+}
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
 	ensure => running,
-	require => Package['nginx'],
+	require => Package['nginx']
 }
