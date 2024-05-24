1ced68623910cde9da8ba2aa6a9f56be70aa7bc4
njoking <njokingugi9462@gmail.com>
2024-05-24 02:52:01 -0700

    lets go

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+#automate nginx using puppet
+
+
+package { 'nginx':
+	ensure => installed,
+}
+file_line { 'install':
+	ensure => 'present',
+	path => '/etc/nginx/sites-enabled/default',
+	after => 'listen 80 default_server;',
+	line => 'rewrite ^/redirect_me https://www.github.com/njoking permnent;',
+}
+
+file { '/var/www/html/index.html':
+	content => 'Hello World!'
+	}
+service { 'nginx' :
+	ensure => running,
+	require => Package['nginx'],
+}
